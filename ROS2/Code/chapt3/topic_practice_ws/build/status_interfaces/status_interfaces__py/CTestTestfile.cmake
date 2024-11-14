283fb39c67046cf3a7af53d2c165b16ec1c8c307
niechao <niechao5663@163.com>
2024-11-14 23:31:55 +0800

    update

---
+++ ROS2/Code/chapt3/topic_practice_ws/build/status_interfaces/status_interfaces__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cat/WorkSpace/Learning/ROS2/Code/chapt3/topic_practice_ws/build/status_interfaces/status_interfaces__py
+# Build directory: /home/cat/WorkSpace/Learning/ROS2/Code/chapt3/topic_practice_ws/build/status_interfaces/status_interfaces__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
