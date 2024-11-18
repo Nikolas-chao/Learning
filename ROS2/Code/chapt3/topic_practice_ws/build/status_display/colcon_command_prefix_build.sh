8dd7c5f50d090640b7c8bf67e04955f52ca60e81
com22 <niechao5663@163.com>
2024-11-18 16:44:23 +0800

    com update

---
+++ ROS2/Code/chapt3/topic_practice_ws/build/status_display/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/cat/WorkSpace/Learning/ROS2/Code/chapt3/topic_practice_ws/install/status_interfaces/share/status_interfaces/package.sh"
